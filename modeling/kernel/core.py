5d0653e01ff8485763118738f51d2db4bf6693e0
ursinedeity <huanan1991@gmail.com>
2017-10-31 15:54:14 -0700

    start building up modeling kernel

---
+++ modeling/kernel/core.py
@@ -0,0 +1,26 @@
+from __future__ import division, absolute_import, print_function
+
+
+class System(Object):
+    """
+    
+    Modeling target system
+    
+    """
+    
+    def addParticle():
+        """
+        Add particle to system
+        """
+        pass
+    
+    def getParticle():
+        """
+        Get particle position in the system
+        """
+        
+        pass
+    
+#=
+
+
