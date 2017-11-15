71cabc89bfefe6140e3d94e0ee88c1de053e074f
ursinedeity <huanan1991@gmail.com>
2017-11-14 17:20:13 -0800

    put config_test.json as default

--- igm_test.py
+++ igm_test.py
@@ -3,6 +3,7 @@ from alabtools.utils import Index
 import numpy as np
 
 model = igm.model.Model()
+cfg = igm.Config("config_test.json")
 
 for i in range(500):
     model.addParticle(np.random.randn(3)*500, 200, 0)
@@ -19,5 +20,5 @@ pp = igm.restraints.Polymer(index)
 model.addRestraint(pp)
 
 import igm.kernel.lammps
-cfg = igm.Config("testcfg.json")
+
 info = igm.kernel.lammps.optimize(model, cfg.optimization)
