04993ae1472c89d6d8e28fbd77a87506e43ca3c2
Francesco Musella <fmusella@login3.hoffman2.idre.ucla.edu>
2023-04-12 16:21:10 -0700

    Tutorial 1 commit

---
+++ igm_tutorial/speckles_file_creation.py
@@ -0,0 +1,20 @@
+import numpy as np
+import pickle as pkl
+
+# Create three speckles, i.e. sphere with a set of 3D coordinates and a radius
+spe_1 = (np.array([0, 0, 0]), 300)  # center and radius
+spe_2 = (np.array([1500, 0, 0]), 200)
+spe_3 = (np.array([0, 1500, 0]), 500)
+
+spe_lst = [spe_1, spe_2, spe_3]  # list of speckles
+
+# in general, we would have a different list for each structure
+# for this tutorial, we will use the same list for all structures
+
+n_structures = 100  # number of structures
+
+# create a list of lists of speckles
+speckles = [spe_lst for i in range(n_structures)]
+
+# save the list of lists of speckles as a pickle file
+pkl.dump(speckles, open('speckles.pkl', 'wb'))
